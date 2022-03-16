dfa10fd536e0ee1f4f64b1cbafc1064d838a6675
hammer3344 <stephan.l.martin6@gmail.com>
2022-03-16 11:51:25 -0600

    Update SNMP.py
    Updated to add Username prompt and added System Serial Number

--- SNMP.py
+++ SNMP.py
@@ -9,6 +9,7 @@ from pysnmp.smi.rfc1902 import ObjectIdentity
 
 # Secure AutH/PRIV Storage
 
+GET_USER = getpass.getuser('Enter SNMP User: ')
 GET_AUTH = getpass.getpass('Enter Auth Passphrase: ')
 GET_PRIV = getpass.getpass('Enter Priv Passphrase: ')
 
@@ -16,7 +17,7 @@ GET_PRIV = getpass.getpass('Enter Priv Passphrase: ')
 #SNMP Function
 def get_info_snmp(host, oid):
     for (errorIndication,errorStatus,errorIndex,varBinds) in nextCmd(SnmpEngine(),
-    UsmUserData('PYTHON_USR', authKey=GET_AUTH, privKey=GET_PRIV, authProtocol=usmHMACSHAAuthProtocol, privProtocol=usmAesCfb192Protocol),
+    UsmUserData(GET_USER, authKey=GET_AUTH, privKey=GET_PRIV, authProtocol=usmHMACSHAAuthProtocol, privProtocol=usmAesCfb192Protocol),
     UdpTransportTarget((host, 161), timeout=3.0, retries=2),
     ContextData(),
     ObjectType(ObjectIdentity(oid)), lookupMib=False, lexicographicMode=False):
@@ -53,6 +54,12 @@ with open(Addr_File) as read_obj:
         output_list.append(hostname_output)
         output_list.append(ipAddr)
 
+        # Serial Number MIB + output processing
+        serial_mib = get_info_snmp(ipAddr, '1.3.6.1.2.1.47.1.1.1.1.11')
+        serial_output = serial_mib.replace('1.3.6.1.2.1.47.1.1.1.1.11.1 = ', '')
+        output_list.append(serial_output)
+
+
         # Version MIB + output processing
         version_mib = get_info_snmp(ipAddr, '1.3.6.1.2.1.1.1')
         version_match = re.findall('Cisco.+', version_mib)
@@ -61,12 +68,9 @@ with open(Addr_File) as read_obj:
         version_list = version_strip.split(', ')
         final_list = output_list + version_list
 
-        print(final_list)
-
         #Write CSV File
         csv_write = open('/home/steve/Documents/output.csv', 'a', newline='')
         with csv_write:
             write = csv.writer(csv_write)
             write.writerow(final_list)
             csv_write.close()
-
