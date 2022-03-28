cb79b77f36504ecd73e7dc0e7181f60af0f52cd8
hammer3344 <stephan.l.martin6@gmail.com>
2022-03-27 20:34:40 -0600

    Update NMT.py
    massive overhaul of general code design. Transition to class based

--- NMT.py
+++ NMT.py
@@ -4,77 +4,88 @@ from tkinter import messagebox
 from tkinter import filedialog
 
 
-def about():
-    messagebox.showinfo("About", "Written by Stephan Martin\nVersion: 0.5\nDate: 26 March 2022")
+class NetMain:
+
+    def __init__(self):
+        # Main Window Session
+        self.master_window = Tk()
+        self.master_window.title('Network Management Tool')
+        self.master_window.geometry("450x150")
+        self.buttons_frame = Frame(self.master_window)
+        self.buttons_frame.grid(row=0, column=0, sticky=W+E)
+        self.btn_reports = Button(self.buttons_frame, text='Reports', command=self.report_call)
+        self.btn_reports.grid(row=0, column=0, padx=15, pady=15)
+        self.btn_file = Button(self.buttons_frame, text='Real Time Monitor')
+        self.btn_file.grid(row=0, column=1, padx=15, pady=15)
+        self.btn_about = Button(self.buttons_frame, text='About', command=self.about)
+        self.btn_about.grid(row=0, column=3, padx=15, pady=15)
+        self.btn_close = Button(self.buttons_frame, text='Close', command=self.master_window.destroy)
+        self.btn_close.grid(row=0, column=4, padx=15, pady=15)
+        mainloop()
+
+    def about(self):
+        messagebox.showinfo("About", "Written by Stephan Martin\nVersion: 0.5\nDate: 26 March 2022")
+
+    def report_call(self):
+        NetReport()
 
 
-def report_input():
-    filename = filedialog.askopenfilename(initialdir="/", title="Select source CSV", filetypes=(("CSV Files", "*.csv*"),
-    ("all files", "*.*")))
+class NetReport:
+    # Report Page - pop open
+    def __init__(self):
+        self.report_window = Tk()
+        self.report_window.title('Network Management Reports')
+        self.report_window.attributes('-topmost', 1)
+        self.report_frame = Frame(self.report_window)
+        self.report_frame.grid(row=0, column=0, sticky=W+E)
+        self.report_g1 = LabelFrame(self.report_frame, text="Select Options", padx=5, pady=5)
+        self.report_g1.grid(row=0, column=0, columnspan=1, padx=10, pady=10)
+        self.report_window.columnconfigure(0, weight=1)
+        self.report_window.rowconfigure(0, weight=1)
+        self.report_g1.rowconfigure(0, weight=1)
+        self.report_g1.columnconfigure(0, weight=1)
+
+        self.reports_c1 = Checkbutton(self.report_g1, text='Hostname', onvalue=1, offvalue=0)
+        self.reports_c1.grid(row=0, column=0, padx=15, pady=2, sticky=W)
+
+        self.reports_c2 = Checkbutton(self.report_g1, text='IP Address', onvalue=1, offvalue=0)
+        self.reports_c2.grid(row=1, column=0, padx=15, pady=2, sticky=W)
+
+        self.reports_c3 = Checkbutton(self.report_g1, text='Serial Number', onvalue=1, offvalue=0)
+        self.reports_c3.grid(row=2, column=0, padx=15, pady=2, sticky=W)
+
+        self.reports_c4 = Checkbutton(self.report_g1, text='Version', onvalue=1, offvalue=0)
+        self.reports_c4.grid(row=3, column=0, padx=15, pady=2, sticky=W)
+
+        self.report_g2 = LabelFrame(self.report_frame, text="File input", padx=5, pady=5)
+        self.report_g2.grid(row=4, column=0, padx=10, pady=10)
+        self.report_window.columnconfigure(0, weight=1)
+        self.report_window.rowconfigure(4, weight=1)
+        self.report_g2.rowconfigure(4, weight=1)
+        self.report_g2.columnconfigure(0, weight=1)
+
+        self.reports_file = Button(self.report_g2, text='Select File', command=self.report_input)
+        self.reports_file.grid(row=5, column=0, padx=5, pady=5, sticky=W)
+
+        self.report_run = Button(self.report_frame, text='Run')
+        self.report_run.grid(row=5, column=0, padx=10, pady=10)
+
+        self.txtbox = Text(self.report_g2, width=50, height=1)
+        self.txtbox.grid(row=5, column=1, sticky=E + W + N + S)
+
+        self.report_close = Button(self.report_frame, text='Close', command=self.report_window.destroy)
+        self.report_close.grid(row=5, column=3, sticky=E, padx=15, pady=15)
+        self.report_window.mainloop()
+
+    def report_input(self):
+        filename = filedialog.askopenfilename(parent=self.report_window, initialdir="/home/steve/Documents", title="Select source CSV", filetypes=(("CSV Files", "*.csv*"),
+        ("all files", "*.*")))
+        self.path_update(filename)
+
+    def path_update(self, path):
+        self.txtbox.insert(END, path)
 
 
-def reports():
-    report_window = Tk()
-    report_window.title('Reports')
-    #report_window.wm_attributes('-topmost', 1)
-    report_frame = Frame(report_window)
-    report_frame.grid(row=0, column=0, sticky=W+E)
-    report_g1 = LabelFrame(report_frame, text="Select Options", padx=5, pady=5)
-    report_g1.grid(row=0, column=0, columnspan=1, padx=10, pady=10)
-    report_window.columnconfigure(0, weight=1)
-    report_window.rowconfigure(0, weight=1)
-    report_g1.rowconfigure(0, weight=1)
-    report_g1.columnconfigure(0, weight=1)
 
-    reports_c1 = Checkbutton(report_g1, text='Hostname', onvalue=1, offvalue=0)
-    reports_c1.grid(row=0, column=0, padx=15, pady=2, sticky=W)
-
-    reports_c2 = Checkbutton(report_g1, text='IP Address', onvalue=1, offvalue=0)
-    reports_c2.grid(row=1, column=0, padx=15, pady=2, sticky=W)
-
-    reports_c3 = Checkbutton(report_g1, text='Serial Number', onvalue=1, offvalue=0)
-    reports_c3.grid(row=2, column=0, padx=15, pady=2, sticky=W)
-
-    reports_c4 = Checkbutton(report_g1, text='Version', onvalue=1, offvalue=0)
-    reports_c4.grid(row=3, column=0, padx=15, pady=2, sticky=W)
-
-    report_g2 = LabelFrame(report_frame, text="File input", padx=5, pady=5)
-    report_g2.grid(row=4, column=0, padx=10, pady=10)
-    report_window.columnconfigure(0, weight=1)
-    report_window.rowconfigure(4, weight=1)
-    report_g2.rowconfigure(4, weight=1)
-    report_g2.columnconfigure(0, weight=1)
-
-    reports_file = Button(report_g1, text='Select File', command=report_input)
-    reports_file.grid(row=5, column=0, padx=5, pady=5, sticky=W)
-
-    report_run = Button(report_frame, text='Run')
-    report_run.grid(row=5, column=0, padx=15, pady=15)
-
-    #txtbox = scrolledtext.ScrolledText(group1, width=40, height=10)
-    #txtbox.grid(row=0, column=0, sticky=E + W + N + S)
-
-    report_close = Button(report_frame, text='Close', command=report_window.destroy)
-    report_close.grid(row=5, column=3, sticky=E, padx=15, pady=15)
-    report_window.mainloop()
-
-
-def main():
-    master_window = Tk()
-    master_window.title('Network Management Tool')
-    master_window.geometry("450x150")
-    # Parent widget for the buttons
-    buttons_frame = Frame(master_window)
-    buttons_frame.grid(row=0, column=0, sticky=W+E)
-    btn_reports = Button(buttons_frame, text='Reports', command=reports)
-    btn_reports.grid(row=0, column=0, padx=15, pady=15)
-    btn_file = Button(buttons_frame, text='Real Time Monitor')
-    btn_file.grid(row=0, column=1, padx=15, pady=15)
-    btn_about = Button(buttons_frame, text='About', command=about)
-    btn_about.grid(row=0, column=3, padx=15, pady=15)
-    btn_close = Button(buttons_frame, text='Close', command=master_window.destroy)
-    btn_close.grid(row=0, column=4, padx=15, pady=15)
-    mainloop()
-
-
-main()
+# instantiate session
+app = NetMain()
